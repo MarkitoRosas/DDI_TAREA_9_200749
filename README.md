cd185847e58bdabb03e0cc5989f480a3c9b63be5
MarkitoRosas <marcoantonio.rosasgonzalez@utxicotepec.edu.mx>
2023-07-20 00:07:32 -0600

    TAREA 9

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# DDI_TAREA_9_200749
+# DDI_Tarea_9_200749
+
+[![Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png](https://i.postimg.cc/15q3LFXF/Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png)](https://postimg.cc/MvzwBvyZ)
+
+<div align="center">
+  
+# Universidad Tecnológica de Xicotepec de Juárez
+
+
+## Ingeniería en Desarrollo y Gestión de Software
+## Marco Antonio Rosas González
+## 9A
+### Desarrollo de Dispositivos Inteligentes
+
+
+
+
+&nbsp;
+&nbsp;
+|  Característica |  Información |
+| :------------: | :------------: |
+| Nombre | **Tarea 9** *Geoposicionamiento* |
+| Descripción  | Obtención del geoposicionamiento actual del dispositivo Wearable.  |
+|  Fecha de Carga | 24/05/23  |
---
+++ app/src/main/java/com/example/tarea7/presentation/MainActivity.kt
@@ -0,0 +1,161 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.example.tarea7.presentation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.example.tarea7.R
+import com.example.tarea7.presentation.theme.Tarea7Theme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : ComponentActivity() {
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var locationCallback: LocationCallback? = null
+    private var mHandler: Handler? = null
+    private var mRunnable: Runnable? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activitymain)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult == null) {
+                    return
+                }
+                for (location in locationResult.locations) {
+                    // Aquí obtienes la ubicación actual
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    // Haz lo que necesites con la ubicación
+                    val saludosTextView = findViewById<TextView>(com.example.tarea7.R.id.Saludo)
+                    val lolTextView = findViewById<TextView>(com.example.tarea7.R.id.lol)
+
+                    saludosTextView.text = "Latitud: $latitude"
+                    lolTextView.text = "Longitud: $longitude"
+                }
+            }
+        }
+
+        mHandler = Handler()
+        mRunnable = Runnable {
+            /*updateTime()
+            mRunnable?.let { mHandler?.postDelayed(it, 1000) }*/ // Actualiza cada segundo
+        }
+
+    }
+
+
+    override fun onResume() {
+        super.onResume();
+        startLocationUpdates();
+        mRunnable?.let { mHandler?.post(it) }
+    }
+
+    override fun onPause() {
+        super.onPause();
+        stopLocationUpdates();
+        mRunnable?.let { mHandler?.removeCallbacks(it) }
+    }
+
+    private fun startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val locationRequest: LocationRequest = LocationRequest.create()
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            locationRequest.setInterval(5000) // Intervalo en milisegundos para obtener actualizaciones de ubicación
+            fusedLocationClient!!.requestLocationUpdates(locationRequest, locationCallback, null)
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient!!.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            }
+        }
+    }
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    Tarea7Theme {
+
+        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+         * version of LazyColumn for wear devices with some added features. For more information,
+         * see d.android.com/wear/compose.
+         */
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Greeting(greetingName = greetingName)
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world, greetingName)
+    )
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
